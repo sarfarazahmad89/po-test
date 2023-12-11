f83f1c65fb19cf19207af7aec42ef4aa75acc8f7
Sarfaraz Ahmad <ahmad.sarfaraz1989@gmail.com>
2023-12-11 02:16:20 +0100

    Add support for mandating labels and annotations

--- pkg/tests/unittest.go
+++ pkg/tests/unittest.go
@@ -26,7 +26,88 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
+
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/model/rulefmt"
+	"golang.org/x/exp/slices"
 )
+
+var (
+	mandatoryLabels      = []string{"owner", "severity"}
+	mandatoryAnnotations = []string{"runbook_url", "description", "summary"}
+)
+
+type RuleGroup struct {
+	Name     string         `yaml:"name"`
+	Interval model.Duration `yaml:"interval,omitempty"`
+	Limit    int            `yaml:"limit,omitempty"`
+	Rules    []rulefmt.Rule `yaml:"rules"`
+}
+
+type Rule struct {
+	Record        string            `yaml:"record,omitempty"`
+	Alert         string            `yaml:"alert,omitempty"`
+	Expr          string            `yaml:"expr"`
+	For           model.Duration    `yaml:"for,omitempty"`
+	KeepFiringFor model.Duration    `yaml:"keep_firing_for,omitempty"`
+	Labels        map[string]string `yaml:"labels,omitempty"`
+	Annotations   map[string]string `yaml:"annotations,omitempty"`
+}
+
+type RuleGroups struct {
+	Groups []RuleGroup `yaml:"groups"`
+}
+
+type Spec struct {
+	Spec RuleGroups `yaml:"spec"`
+}
+
+func getLabels(ruleDef rulefmt.Rule) []string {
+	presentLabels := []string{}
+	for k := range ruleDef.Labels {
+		presentLabels = append(presentLabels, k)
+	}
+	return presentLabels
+}
+
+func getAnnotations(ruleDef rulefmt.Rule) []string {
+	presentLabels := []string{}
+	for k := range ruleDef.Labels {
+		presentLabels = append(presentLabels, k)
+	}
+	return presentLabels
+}
+
+func inspectRule(ruleDef rulefmt.Rule) error {
+	var errored bool
+	missingLabels := []string{}
+	missingAnnotations := []string{}
+	presentLabels := getLabels(ruleDef)
+	for _, label := range mandatoryLabels {
+		if !slices.Contains(presentLabels, label) {
+			missingLabels = append(missingLabels, label)
+		}
+	}
+	for _, annotation := range mandatoryAnnotations {
+		if !slices.Contains(getAnnotations(ruleDef), annotation) {
+			missingAnnotations = append(missingAnnotations, annotation)
+		}
+	}
+
+	if len(missingLabels) > 0 {
+		fmt.Println("missing mandatory labels: ", strings.Join(missingLabels, ", "))
+		errored = true
+	}
+	if len(missingAnnotations) > 0 {
+		fmt.Println("missing mandatory annotations:  ", strings.Join(missingAnnotations, ", "))
+		errored = true
+	}
+	if errored {
+		return fmt.Errorf("missing mandatory labels/annotations")
+	}
+	return nil
+}
 
 func RunUnitTests(testFiles []string) error {
 	var originalRules []*filenameAndData
@@ -55,6 +136,21 @@ func RunUnitTests(testFiles []string) error {
 				return err
 			}
 
+			var structuredSpec Spec
+			err = yaml.Unmarshal(yamlFile, &structuredSpec)
+			if err != nil {
+				return err
+			}
+
+			for _, ruleGroup := range structuredSpec.Spec.Groups {
+				for _, ruleDef := range ruleGroup.Rules {
+					err = inspectRule(ruleDef)
+					if err != nil {
+						return err
+					}
+					fmt.Printf("%v", ruleDef.Labels)
+				}
+			}
 			if spec, found := unstructured["spec"]; found {
 				ruleFileContentWithoutMetadata, err := yaml.Marshal(spec)
 				if err != nil {
